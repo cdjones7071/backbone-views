af6462bd2246edad7dcc71db7b536c5bc75ea2ad
Carlos Jones <carlosjones@Carloss-MBP.home>
2015-08-14 14:08:24 -0400

    First commit

--- public/card.js
+++ public/card.js
@@ -1,18 +1,60 @@
-// Using a common eventDispatcher for this project is a good idea.
-var eventDispatcher = _.clone(Backbone.Events)
 
+var firstCard;
+var secondCard;
+var firstCardColor;
+var secondCardColor;
+var numberedClicks = 0
 
-// todo
 var Card = Backbone.View.extend({
+ 
+  className: "card",
+   
+   events: {
 
-  // more todo
-  events: {
+  	"click": "Cardflipping",
+   },
+ 
 
+  initialize: function(viewData) {
+  	
+    this.render(viewData)
+ 
   },
 
-  // also todo
-  initialize: function(viewData) {
-    
-  }
+  render: function(viewData) {
+  	this.color = viewData.color;
+    this.hints = viewData.hints;
+    this.answers = viewData.answers
+  },
 
-})
+  Cardflipping: function() {
+  	numberedClicks = 1;
+    this.$el.css('background-color', this.color);
+     
+    if (numberedClicks === 1) {
+    	firstCard = this.$el;
+    	firstCardColor = this.$el.css( "background-color" );
+    }
+    if (numberedClicks === 2) {
+    	secondCard = this.$el;
+    	secondCardColor = this.$el.css( "background-color" );
+    	if (firstCardColor === secondCardColor) {
+    		firstCard.remove();
+    		secondCard.remove();
+    	} else {
+    		alert("wtffff");
+    	}
+    }
+    if (numberedClicks === 3) {
+
+    	numberedClicks = 0;
+    	$(".card").css('background-color', 'red')
+    }
+   },
+
+   template: Handlebars.compile( $("#bank-template").html() )
+}) 
+
+
+
+
